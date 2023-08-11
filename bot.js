09ec00cacb424974ffab5c00db156a90ea537dfe
eelel <67670160+The-WindowsVista@users.noreply.github.com>
2023-08-11 05:33:29 +0000

    Add files via upload
    replace server emojis with placeholders

--- bot.js
+++ bot.js
@@ -60,15 +60,15 @@ const channel = await interaction.client.channels.fetch(channelId);
 const secondHalfMessage = await channel.send(secondHalf);
 
 // React to the second half message
-await secondHalfMessage.react('<:f_:1139350834778480680>');
-await secondHalfMessage.react('<:l_:1139351570522329240>');
+await secondHalfMessage.react('F');
+await secondHalfMessage.react('L');
   }
 });
 
 client.on('interactionCreate', async (interaction) => {
   if (!interaction.isCommand()) return;
   if (interaction.commandName === 'help') {
-  await interaction.reply("Minesweeper Rules:\n1. If you find a <:x_:1139345434054242344>, you lose.\n2. Please don't cheat, if you post the solution, you may be banned depending on the server's mods.\n3. Once you reveal every tile that isn't a mine (in this 9x9 board, there is 10), you win.\n4. React <:f_:1139350834778480680> if you win, and <:l_:1139351570522329240> if you lose.\n5. The top left corner is always safe, so start there.");
+  await interaction.reply("Minesweeper Rules:\n1. If you find a X, you lose.\n2. Please don't cheat, if you post the solution, you may be banned depending on the server's mods.\n3. Once you reveal every tile that isn't a mine (in this 9x9 board, there is 10), you win.\n4. React F if you win, and L if you lose.\n5. The top left corner is always safe, so start there.");
     }
 });
 
@@ -137,10 +137,10 @@ function generateMinesweeperBoard() {
 // Function to convert board to Discord emojis
 function convertToDiscordEmojis(board) {
   const emojis = [
-    '||<:__:1139345030612537375>||', // 0
-    '||<:1_:1139344548083019836>||', '||<:2_:1139345260019974214>||', '||<:3_:1139350629672833114>||', '||<:4_:1139351457112535122>||', '||<:5_:1139352253178843218>||', '||<:6_:1139352702216851518>||', '||<:7_:1139352704091705414>||', '||<:8_:1139352255485718678>||' // 1-8
+    '||0||', // 0
+    '||1||', '||2||', '||3||', '||4||', '||5||', '||6||', '|7||', '|8||' // 1-8
   ];
-  const mineEmoji = '||<:x_:1139345434054242344>||';
+  const mineEmoji = '||X||';
   const discordBoard = [];
 
   for (let i = 0; i < board.length; i++) {
